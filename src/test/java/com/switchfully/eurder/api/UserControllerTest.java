1c789b0c993a17bb4b5bc577bc8c2bd3f854dd2d
fmkarakus <fatihkarakus@hotmail.com>
2022-10-28 09:37:55 +0200

    happy path tests added

--- src/test/java/com/switchfully/eurder/api/UserControllerTest.java
+++ src/test/java/com/switchfully/eurder/api/UserControllerTest.java
@@ -1,9 +1,12 @@
 package com.switchfully.eurder.api;
 
 import com.switchfully.eurder.api.dtos.CustomerDto;
+import com.switchfully.eurder.repositories.ItemRepository;
+import com.switchfully.eurder.repositories.UserRepository;
 import io.restassured.http.ContentType;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -11,14 +14,20 @@ import org.springframework.test.annotation.DirtiesContext;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class UserControllerTest {
-
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ItemRepository itemRepository;
     @LocalServerPort
     private int port;
 
-    private static String requestBody =String.format( """
+    @Test
+    void addCustomer() {
+        String requestBody = String.format("""
             {
               "firstName": "fname",
               "lastName": "lname",
@@ -30,10 +39,8 @@ class UserControllerTest {
               "phoneNumber": "04653122246",
               "password": "password"
             }
-            ""","3500");
-    @Test
-    void addCustomer() {
-        CustomerDto response= given()
+            """, "3500");
+        CustomerDto response = given()
                 .baseUri("http://localhost")
                 .port(port)
                 .header("Content-type", "application/json")
@@ -47,5 +54,59 @@ class UserControllerTest {
                 .extract()
                 .as(CustomerDto.class);
         Assertions.assertThat(response.getFirstName()).isEqualTo("fname");
+    }
+
+    @Test
+    void viewAllCustomers() {
+        given()
+                .baseUri("http://localhost")
+                .port(port)
+                .auth()
+                .preemptive()
+                .basic("admin@eurder.com", "password")
+                .header("Accept", "application/json")
+                .header("Content-type", "application/json")
+                .and()
+                .when()
+                .get("/customers")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.OK.value())
+                .extract();
+    }
+
+    @Test
+    void addOrder() {
+        String aUserId = userRepository.getPersonbyEmail("customer@test.be").get().getId();
+        String itemId1 = itemRepository.getItemMap().values().stream().filter(item -> item.getName().equals("item1")).findFirst().get().getId();
+        String itemId2 = itemRepository.getItemMap().values().stream().filter(item -> item.getName().equals("item2")).findFirst().get().getId();
+        String requestBody = String.format("""
+                [
+                  {
+                    "itemId": "%s",
+                    "amount": 2
+                  },
+                    {
+                    "itemId": "%s",
+                    "amount": 2
+                  }
+                ]
+                """, itemId1, itemId2);
+        given()
+                .baseUri("http://localhost")
+                .port(port)
+                .auth()
+                .preemptive()
+                .basic("customer@test.be", "password")
+                .header("Accept", "application/json")
+                .header("Content-type", "application/json")
+                .and()
+                .body(requestBody)
+                .when()
+                .post("/customers/" + aUserId + "/order")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.CREATED.value())
+                .extract();
     }
 }
